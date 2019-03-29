8f6bc3baf7098c0fe24684ce1ce2d893a60b96e4
Elan Green <green.elan5@gmail.com>
2019-03-29 11:24:56 -0500

    add functions to dynamically access elements in the store array

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const STORE = [
   {name: 'bread', checked: false}
 ];
 
-function renderShoppingList() {
+function generateItemElement(item, itemIndex, template) {
+  return `
+  <li>${item.name}</li>`;
+}
 
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+
+  return items.join('');
+}
+
+function renderShoppingList() {
+  // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+  $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 function handleNewItemSubmit() {
